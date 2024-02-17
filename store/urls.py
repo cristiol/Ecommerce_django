9bb4c431cad353e153f6819bcc741446b6719bfe
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-02-17 12:00:50 +0200

    added templating out and about page

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.home , name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
 ]
