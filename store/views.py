31484df32a0d05a9a185119ca852e208301863ce
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-02-18 14:07:43 +0200

    add register

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 
 def home(request):
@@ -33,4 +37,24 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out')
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have been registered')
+            return redirect('home')
+        else:
+            messages.success(request, 'There was a problem')
+            return redirect('register')
+
+    return render(request, 'register.html', {"form": form})
 
