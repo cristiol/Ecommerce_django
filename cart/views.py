0c79ae70dd06777d479ac732f837caea606bbf17
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-02-19 23:03:51 +0200

    added quantities

--- cart/views.py
+++ cart/views.py
@@ -8,7 +8,8 @@ from django.contrib import messages
 def cart_summary(request):
     cart = Cart(request)
     cart_products = cart.get_prods()
-    return render(request, 'cart_summary.html', {'cart_products': cart_products})
+    quantities = cart.get_quants()
+    return render(request, 'cart_summary.html', {'cart_products': cart_products, 'quantities': quantities})
 
 
 def cart_add(request):
@@ -18,10 +19,11 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         # get the stuff
         product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
         # look up for product in db
         product = get_object_or_404(Product, id=product_id)
         # save the session
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_qty)
         # get cart quantity
         cart_quantity = cart.__len__()
 
