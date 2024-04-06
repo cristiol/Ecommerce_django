b676384152cced973cde6e7614f1180b09fdacd4
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-04-06 11:12:30 +0300

    add order model

--- payments/models.py
+++ payments/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+from store.models import Product
+
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -30,3 +32,32 @@ def create_shipping(sender, instance, created, **kwargs):
 
     # Automate the profile thing
     post_save.connect(create_shipping, sender=User)
+
+
+# Create Order Model
+class Order(models.Model):
+    # Foreign Key
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order - {str(self.id)}'
+
+
+# Create Order Items Model
+class OrderItem(models.Model):
+    # Foreign Keys
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+    def __str__(self):
+        return f'Order Item - {str(self.id)}'
