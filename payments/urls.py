80f009adadd2114978dbe3b371c638a81b9b02f8
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-04-06 11:51:38 +0300

    add chekcout

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import payments_success
+from .views import payments_success, checkout, billing_info
 
 
 urlpatterns = [
-    path('payments-success', payments_success, name='payments_success')
+    path('payments-success', payments_success, name='payments_success'),
+    path('checkout', checkout, name='checkout'),
+    path('billing_info', billing_info, name="billing_info"),
 ]
