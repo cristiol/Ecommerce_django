e89aad3ac3df71d8ff74a41c32f6d9b17ff0adc6
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-06-12 15:18:50 +0300

    add feature order

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import payments_success, checkout, billing_info
+from .views import payments_success, checkout, billing_info, process_order
 
 
 urlpatterns = [
     path('payments-success', payments_success, name='payments_success'),
     path('checkout', checkout, name='checkout'),
     path('billing_info', billing_info, name="billing_info"),
+    path('process_order', process_order, name="process_order")
 ]
