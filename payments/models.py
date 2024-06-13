301598ee254d14cfbd8f4c818b52ceddd269d558
Cristi Olteanu <cristiolteanu1892@gmail.com>
2024-06-13 15:38:01 +0300

    add shipped

--- payments/models.py
+++ payments/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+import datetime
 
 from store.models import Product
 
@@ -43,9 +45,21 @@ class Order(models.Model):
     shipping_address = models.TextField(max_length=15000)
     amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
+    shipped = models.BooleanField(default=False)
+    date_shipped = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f'Order - {str(self.id)}'
+
+
+# Auto Add shipping Date
+@receiver(pre_save, sender=Order)
+def set_shipped_date_on_update(sender, instance, **kwargs):
+    if instance.pk:
+        now = datetime.datetime.now()
+        obj = sender._default_manager.get(pk=instance.pk)
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
 
 
 # Create Order Items Model
